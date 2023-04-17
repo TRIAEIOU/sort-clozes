c2866562a42cd525075ae310df2999fbcfb75733
TRIAEIOU <94647023+TRIAEIOU@users.noreply.github.com>
2023-04-18 00:52:52 +0200

    Initial

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Sort clozes
 
-Anki addon to sort clozes in notes in ascending order while keeping stats/learning state.
+[Anki](https://apps.ankiweb.net) addon ([GitHub](https://github.com/TRIAEIOU/sort-clozes)) to sort clozes in notes in ascending order while keeping stats/learning state.
+
+## Warning
+
+I have tested this addon to a reasonable extent, however take care if you have large collections with long running stats. Make a backup (the addon creates one as well for this reason) and try on a few notes to verify it does what you want before running it on the root of your collection. I made this for my personal use and share it so that others may benefit if it suits them, I do not take responsibility for anything!
 
 ## Use
 
-Use `Sort clozes` from main window toolbar `Tools` or from context menu in the Browser sidebar or table (requires 1+ notes/cards to be selected) to sort all cloze notes in selected deck (including subdecks)/selection.
+Use `Sort clozes` from main window toolbar `Tools` or from context menu in the Browser sidebar or table (requires 1+ notes/cards to be selected) to sort all cloze notes in selection/selected deck (including subdecks).
 
-- The addon will sort all clozes on all cloze type notes with at least one card in the selection.
+- The addon will sort all clozes on all cloze notes with at least one card in the selection.
+- Note that the note/card table is not updated, select another deck (so that the table updates) to see the result.
 - A backup is created automatically prior to sorting (unless no changes has been made since the last backup).
--- bin/__init__.py
+++ bin/__init__.py
@@ -1,37 +1,56 @@
-import re, anki, aqt
+import re
+from anki import notes, collection
+from aqt import mw, qt, operations, browser, gui_hooks
 from anki.consts import *
+from .version import *
 
 TITLE = "Sort clozes"
+NVER = "1.0.0"
 
 #########################################################################
-def sort_notes(parent: aqt.qt.QMainWindow, nids: list[anki.notes.NoteId]):
+def sort_notes(parent: qt.QMainWindow, nids: list[notes.NoteId]):
     """Sort clozes on notes in background op, checks for non-cloze notes"""
 
-    def sort(col: anki.collection.Collection, nids: list[anki.notes.NoteId]):
-        changes = anki.collection.OpChanges()
+    def sort(col: collection.Collection, nids: list[notes.NoteId]):
+        changes = collection.OpChanges()
         notes = []
         cards = []
         for nid in nids:
             note = col.get_note(nid)
             if note.note_type().get('type', MODEL_STD) != MODEL_CLOZE: continue
+            #print(":found note " + str(nid))
 
-            note_cards = {str(card.ord + 1): card for card in note.cards()}
+            note_cards = {}
+            for card in note.cards():
+                # db is 0-based, notes 1-based
+                note_cards[str(card.ord + 1)] = {'card': card, 'ord': -1}
+
             i = 0
-
             def substitute(match):
                 nonlocal i, cards
-                i += 1
-                card = note_cards[match.group(1)]
-                card.ord = i - 1
-                cards.append(card)
-                return rf'{{{{c{i}::'
+                # Multicloze cards should only be added once, after that reuse same ord
+                if card := note_cards[match.group(1)]['card']:
+                    card.ord = i # db is 0-based
+                    i += 1 # notes are 1-based
+                    #print(rf":      appending card {str(card.id)}")
+                    cards.append(card)
+                    note_cards[match.group(1)] = {'card': None, 'ord': i}
+                    ord = i
+                else: ord = note_cards[match.group(1)]['ord']
+
+                #print(rf":    returning '{{{{c{ord}::'")
+                return rf'{{{{c{ord}::'
 
             for fld in note.keys():
                 note[fld] = re.sub(r'{{c(\d+)::', substitute, note[fld])
 
+
+            #print(":  appending note " + str(note.id))
             notes.append(note)
 
+        #print(":updating cards: " + str(", ".join([str(t.id) for t in cards])))
         col.update_cards(cards)
+        #print(":updating notes: " + str(", ".join([str(t.id) for t in notes])))
         col.update_notes(notes)
         return changes
 
@@ -39,12 +58,12 @@ def sort_notes(parent: aqt.qt.QMainWindow, nids: list[anki.notes.NoteId]):
         pass
 
     # Create a root backup
-    aqt.mw.col.create_backup(
-        backup_folder=aqt.mw.pm.backupFolder(),
+    mw.col.create_backup(
+        backup_folder=mw.pm.backupFolder(),
         force=True,
         wait_for_completion=True,
     )
-    bgop = aqt.operations.CollectionOp(
+    bgop = operations.CollectionOp(
       parent=parent,
       op=lambda col: sort(col, nids)
     )
@@ -53,10 +72,10 @@ def sort_notes(parent: aqt.qt.QMainWindow, nids: list[anki.notes.NoteId]):
 
 
 #########################################################################
-def sort_table(browser: aqt.browser.Browser, menu: aqt.qt.QMenu):
+def sort_table(browser: browser.Browser, menu: qt.QMenu):
     """Add menu entry on browser table context"""
     if (len(browser.selected_notes()) > 0):
-        action = aqt.qt.QAction(TITLE, menu)
+        action = qt.QAction(TITLE, menu)
         action.triggered.connect(
           lambda: sort_notes(browser, browser.table.get_selected_note_ids())
         )
@@ -65,29 +84,38 @@ def sort_table(browser: aqt.browser.Browser, menu: aqt.qt.QMenu):
 
 
 #########################################################################
-def sort_sidebar(sidebar: aqt.browser.SidebarTreeView, menu: aqt.qt.QMenu,
-                 item: aqt.browser.SidebarItem, index: aqt.qt.QModelIndex):
+def sort_sidebar(sidebar: browser.SidebarTreeView, menu: qt.QMenu,
+                 item: browser.SidebarItem, index: qt.QModelIndex):
     """Add menu entry on browser sidebar context"""
+    def exe():
+        nids = {}
+        for cid in sidebar.col.decks.cids(item.id, True):
+            nid = sidebar.col.get_card(cid).nid
+            nids[nid] = True
+        sort_notes(sidebar.browser, list(nids.keys()))
 
-    if item.item_type == aqt.browser.SidebarItemType.DECK:
-        action = aqt.qt.QAction(TITLE, menu)
-        action.triggered.connect(lambda: sort_notes(
-          sidebar.browser,
-          list(dict([
-            sidebar.col.get_card(cid).nid
-            for cid in sidebar.col.decks.cids(item.id, True)
-          ]))
-        ))
+    if item.item_type == browser.SidebarItemType.DECK:
+        action = qt.QAction(TITLE, menu)
+        action.triggered.connect(exe)
         menu.addAction(action)
     return menu
 
-# Main ##################################################################
-action = aqt.qt.QAction(TITLE, aqt.mw)
-action.triggered.connect(lambda: sort_notes(
-  aqt.mw,
-  aqt.mw.col.decks.cids(aqt.mw.col.decks.get_current_id(), True)
-))
-aqt.mw.form.menuTools.addAction(action)
+#########################################################################
+def sort_toolbar(win):
+    """Add menu entry to main windows toolbar"""
+    def exe():
+        nids = {}
+        for cid in win.col.decks.cids(win.col.decks.get_current_id(), True):
+            nids[win.col.get_card(cid).nid] = True
+        sort_notes(win, list(nids))
 
-aqt.gui_hooks.browser_will_show_context_menu.append(sort_table)
-aqt.gui_hooks.browser_sidebar_will_show_context_menu.append(sort_sidebar)
+    action = qt.QAction(TITLE, win)
+    action.triggered.connect(exe)
+    mw.form.menuTools.addAction(action)
+
+# Main ##################################################################
+if strvercmp(NVER, get_version()) > 0: set_version(NVER)
+
+sort_toolbar(mw)
+gui_hooks.browser_will_show_context_menu.append(sort_table)
+gui_hooks.browser_sidebar_will_show_context_menu.append(sort_sidebar)
